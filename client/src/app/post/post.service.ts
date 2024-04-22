861093629e1f433f86ad4fe59ffb1f27af5ecc11
apple <ishravan919@gmail.com>
2024-04-22 10:43:49 +0530

    second

--- client/src/app/post/post.service.ts
+++ client/src/app/post/post.service.ts
@@ -11,7 +11,7 @@ import { Post } from './post';
 })
 export class PostService {
 
-  private apiURL = "http://localhost:3000/tasks";
+  private apiURL = "https://shravan-das.github.io/client/db.json";
 
   httpOptions = {
     headers: new HttpHeaders({
